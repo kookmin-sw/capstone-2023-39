46e6c89fd2ace2269fa62c96f9b01156510ae6ea
dan-2ee <ekdms7273@naver.com>
2023-03-10 15:16:03 +0900

    delete unused file

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,15 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom' // 라우팅
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css'; //bootstrap을 사용하기 위해
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom"; // 라우팅
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter> 
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
@@ -17,4 +13,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
