e5b8b146a007d861d7e870e962bc29e44fdfb532
leejoowon20151334 <jwlee51@kookmin.ac.kr>
2023-03-29 16:04:12 +0900

    geo/map api elasticsearch 연결

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GeoController } from './geo/geo.controller';
-import { GeoModule } from './geo/geo.module';
 import { GeoService } from './geo/geo.service';
 import { CoinController } from './coin/coin.controller';
 import { CoinService } from './coin/coin.service';
@@ -10,7 +9,7 @@ import { CoinModule } from './coin/coin.module';
 import { SearchModule } from './search/search.module';
 
 @Module({
-  imports: [GeoModule, CoinModule, SearchModule],
+  imports: [CoinModule, SearchModule],
   controllers: [AppController, GeoController, CoinController],
   providers: [AppService, GeoService, CoinService],
 })
--- back-end/src/search/search.service.ts
+++ back-end/src/search/search.service.ts
@@ -3,8 +3,15 @@ import { long } from '@elastic/elasticsearch/lib/api/types';
 import { Injectable } from '@nestjs/common';
 import { ElasticsearchService } from '@nestjs/elasticsearch';
 
-type dataResponse = {
+interface dataResponse {
   //출력구조
+  id : number,
+  ip : string,
+  lat : string,
+  long : string,
+  country : string,
+  city : string,
+  domain : string
 };
 
 @Injectable()
@@ -53,5 +60,40 @@ export class SearchService {
     });
 
     return { results: Array.from(results), total: response.hits.total };
+  }
+
+  async search_range(
+    lat_start:string, lat_end:string, 
+    long_start:string, long_end:string
+    ) {
+    let results = new Set();
+    const response = await this.esService.search({
+      index: '',
+      size: 100,
+      body: {
+        query: {
+          range: {
+            label: {
+              gt: 0
+            }
+          }
+        },
+      },
+    });
+    const hits = response.hits.hits;
+    hits.map((item) => {
+      let result = {
+        id : 1,
+        ip : item._source['source'],
+        lat : "37.6100021",
+        long : "126.9971053",
+        country : "SouthKorea",
+        city : "Seoul",
+        domain : "kookmin.ac.kr"
+      }
+      results.add(result);
+    });
+
+    return {total: response.hits.total, results: Array.from(results)};
   }
 }
