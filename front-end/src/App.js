08430a2bec3c5cf0ff693f73374be751dbaf43e8
dan-2ee <ekdms7273@naver.com>
2023-03-31 21:10:27 +0900

    feat: routing 변경

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Admin from "./pages/Admin/admin";
-import Visualization from "./pages/Visualization/visualization";
 import Join from "./pages/Join/join";
 import Login from "./pages/Login/login";
 import Main from "./pages/Main/main";
-import NewflowAnalysis from "./pages/NetflowAnalysis/netflowAnalysis";
 
 function App() {
   return (
@@ -14,9 +11,6 @@ function App() {
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/join" element={<Join />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/visualization" element={<Visualization />} />
-        <Route path="/newflowAnalysis" element={<NewflowAnalysis />} />
       </Routes>
     </BrowserRouter>
   );
