2d67ea8572f1bfa2ddebab9445a2e26dc78e2d0b
chanjin1998 <chanjin@kookmin.ac.kr>
2023-04-01 17:03:14 +0900

    merge modify

--- front-end/src/pages/Join/join.js
+++ front-end/src/pages/Join/join.js
@@ -1,18 +1,7 @@
 import React from "react";
-import Header from "../../components/Header/header";
-import { Wrap, Contents } from "./styles";
 
 function Join() {
-<<<<<<< HEAD
-  return (
-    <Wrap>
-      <Header title={"Join"}></Header>
-      <Contents />
-    </Wrap>
-  );
-=======
   return <div>join page</div>;
->>>>>>> upstream/frontend
 }
 
 export default Join;
--- front-end/src/pages/Login/login.js
+++ front-end/src/pages/Login/login.js
@@ -1,18 +1,7 @@
 import React from "react";
-import Header from "../../components/Header/header";
-import { Wrap, Contents } from "./styles";
 
 function Login() {
-<<<<<<< HEAD
-  return (
-    <Wrap>
-      <Header title={"Login"}></Header>
-      <Contents />
-    </Wrap>
-  );
-=======
   return <div>login page</div>;
->>>>>>> upstream/frontend
 }
 
 export default Login;
