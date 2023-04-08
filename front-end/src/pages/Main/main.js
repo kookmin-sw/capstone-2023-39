c6659309e0cae8d0e0c25bc2ab8a0fcffafd5125
chanjin1998 <chanjin@kookmin.ac.kr>
2023-04-08 23:59:15 +0900

    feat: dashboard

--- front-end/src/pages/Main/main.js
+++ front-end/src/pages/Main/main.js
@@ -1,8 +1,27 @@
 import React from "react";
-import KibanaDashboard from "../Dashboard/KibanaDashboard";
+import Map from "../Dashboard/Map/Map";
+import Chart from "../Dashboard/Chart/Chart";
+import Value from "../Dashboard/Value/Value";
+import SupervisedTable from "../Dashboard/Table/Supervised/SupervisedTable";
+import UnSupervisedTable from "../Dashboard/Table/UnSupervised/UnSupervisedTable";
+import HybridTable from "../Dashboard/Table/Hybrid/HybridTable";
+import { BodyContainer, ColumnContainer } from "./styles";
 
 function Main() {
-  return <KibanaDashboard />;
+  return (
+    
+      <BodyContainer>
+        <Map />;
+        <ColumnContainer>
+          <Chart />;
+          <Value />;
+        </ColumnContainer>
+        <SupervisedTable/>
+        <UnSupervisedTable/>
+        <HybridTable/>
+      </BodyContainer>
+  
+  );
 }
 
 export default Main;
