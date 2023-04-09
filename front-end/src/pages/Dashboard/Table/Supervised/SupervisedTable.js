dd2396be411facc1227fd139ad3ce340c64fd744
dan-2ee <ekdms7273@naver.com>
2023-04-09 16:16:00 +0900

    style: border-none

--- front-end/src/pages/Dashboard/Table/Supervised/SupervisedTable.js
+++ front-end/src/pages/Dashboard/Table/Supervised/SupervisedTable.js
@@ -8,6 +8,7 @@ const SupervisedTable = () => {
       height="354rem"
       width="35%"
       id="myId"
+      frameBorder={0}
       className="supervisedTable"
       position="relative"
     />
