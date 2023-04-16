fc3d131d73e3ec7d56753a0b481426e9f1656376
dan-2ee <ekdms7273@naver.com>
2023-04-16 19:53:28 +0900

    feat: show modal

--- front-end/src/components/DashboardBottom/index.js
+++ front-end/src/components/DashboardBottom/index.js
@@ -26,6 +26,10 @@ function DashboardBottom() {
       {showSupervised ? (
         <SearchModal close={() => setShowSupervised(false)} />
       ) : null}
+      {showUnsupervised ? (
+        <SearchModal close={() => setShowUnsupervised(false)} />
+      ) : null}
+      {showHybrid ? <SearchModal close={() => setShowHybrid(false)} /> : null}
       <Iframe
         src="http://203.246.112.139:15601/app/dashboards#/view/760bc710-dae6-11ed-8952-470f17d5498f?embed=true&_g=(refreshInterval%3A(pause%3A!t%2Cvalue%3A60000)%2Ctime%3A(from%3Anow-15m%2Cto%3Anow))&show-time-filter=true&hide-filter-bar=true"
         height="700"
