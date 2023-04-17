3d472a02d874a84d49ca01d870207d8501a20e7b
chanjin1998 <chanjin@kookmin.ac.kr>
2023-04-18 01:19:59 +0900

    feat: map button

--- front-end/src/pages/Main/main.js
+++ front-end/src/pages/Main/main.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import DashboardTop from "../../components/DashboardTop";
 import DashboardBottom from "../../components/DashboardBottom";
-import { BodyContainer } from "./styles";
+import DashboardTopGoodIp from "../../components/DashboardTopGoodIp"
+import { BodyContainer,MapButtonContainer,MapButton } from "./styles";
 
 function Main() {
+  const [dashboardValue, setDashboardValue] = useState(true);
+
+  const ShowGoodIp = () => {
+    setDashboardValue(false);
+  };
+
+  const ShowBadIp = () => {
+    setDashboardValue(true);
+  };
+
   return (
     <BodyContainer>
-      <DashboardTop />
+      {dashboardValue === true && <DashboardTop />}
+      {dashboardValue === false && <DashboardTopGoodIp />}
+      <MapButtonContainer>
+        <MapButton onClick={ShowGoodIp}>Good Ip</MapButton>
+        <MapButton onClick={ShowBadIp}>Bad Ip</MapButton>
+      </MapButtonContainer>
       <DashboardBottom />
     </BodyContainer>
   );
