fa92215dc1779625147182d7758a305765f4d761
dan-2ee <ekdms7273@naver.com>
2023-04-29 18:56:32 +0900

    fix: CORS 해결

--- front-end/src/components/Modal/IpInformation/index.js
+++ front-end/src/components/Modal/IpInformation/index.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as S from "./styles";
+import axios from "axios";
 
 function IpInformation(props) {
   const { open, close, ip } = props;
+  const [info, setInfo] = useState("");
+
+  useEffect(() => {
+    const response = axios
+      /* .get(`/search/ip?ip=${ip}`) */
+      .get(`/search/ip?ip=91.236.51.44`)
+      .then(function (response) {
+        console.log("data", response.data);
+        setInfo(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  });
+
   return (
     <S.CustomModal
       title={"Information"}
