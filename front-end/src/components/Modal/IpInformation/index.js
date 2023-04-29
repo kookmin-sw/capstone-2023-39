5342165e3cc2e3d922f0ddb7ea0d13e79ec03cf8
dan-2ee <ekdms7273@naver.com>
2023-04-29 20:10:21 +0900

    feat: ip information card

--- front-end/src/components/Modal/IpInformation/index.js
+++ front-end/src/components/Modal/IpInformation/index.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from "react";
 import * as S from "./styles";
 import axios from "axios";
-import { Card } from "antd";
+import { Card, List, Typography } from "antd";
 
 function IpInformation(props) {
   const { open, close, ip } = props;
   const [info, setInfo] = useState("");
-  const [comlete, setComplete] = useState(false);
+  const [complete, setComplete] = useState(false);
 
   useEffect(() => {
     setComplete(false);
     const response = axios
-      /* .get(`/search/ip?ip=${ip}`) */
+      .get(`/search/ip?ip=${ip}`)
       .get(`/search/ip?ip=91.236.51.44`)
       .then(function (response) {
         setInfo(response.data?.shodan);
@@ -22,6 +22,34 @@ function IpInformation(props) {
       });
   }, [ip]);
 
+  const column = [
+    "ip",
+    "country_code",
+    "country_name",
+    "region_code",
+    "city",
+    "longitude",
+    "latitude",
+    "hostname",
+    "domains",
+    "os",
+    "isp",
+  ];
+
+  const data = [
+    "91.236.51.44",
+    "US",
+    "United States",
+    "CA",
+    "Mountain View",
+    -122.0775,
+    37.4056,
+    "dns.google",
+    "dns.google",
+    "null",
+    "Google LLC",
+  ];
+
   return (
     <S.CustomModal
       title={"Information"}
@@ -30,11 +58,24 @@ function IpInformation(props) {
       closable={true}
       centered
       footer={null}
-      width={900}
-      bodyStyle={{ height: 430 }}
+      width={950}
+      bodyStyle={{ height: 580 }}
     >
       <S.ModalContent>
-        {comlete ? { ip } : <Card loading style={{ height: 400 }} />}
+        {complete ? (
+          <List
+            bordered
+            pagination
+            dataSource={data}
+            renderItem={(item, index) => (
+              <List.Item>
+                <S.InfoItem> {column[index]}</S.InfoItem> {item}
+              </List.Item>
+            )}
+          />
+        ) : (
+          <Card loading style={{ height: 400 }} />
+        )}
       </S.ModalContent>
     </S.CustomModal>
   );
--- front-end/src/components/Modal/IpInformation/styles.js
+++ front-end/src/components/Modal/IpInformation/styles.js
@@ -2,13 +2,21 @@ import styled from "styled-components";
 import Modal from "antd/es/modal/Modal";
 
 export const CustomModal = styled(Modal)`
+  background-color: #243043;
   .ant-modal-header {
   }
   .ant-modal-title {
-    color: #85b4a8;
-    font-size: 23px;
-    padding: 7px 0;
+    color: #000000;
+    font-size: 28px;
   }
 `;
 
-export const ModalContent = styled.div``;
+export const ModalContent = styled.div`
+  margin-top: 20px;
+`;
+
+export const InfoItem = styled.span`
+  background: linear-gradient(to top, #e1dff6 50%, transparent 50%);
+  font-size: 17px;
+  margin-right: 20px;
+`;
