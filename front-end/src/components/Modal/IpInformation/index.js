de206e5e8ecfdefd94ac5014bfc53435345d8fe6
dan-2ee <ekdms7273@naver.com>
2023-04-29 19:26:08 +0900

    feat: loading card

--- front-end/src/components/Modal/IpInformation/index.js
+++ front-end/src/components/Modal/IpInformation/index.js
@@ -1,23 +1,26 @@
 import React, { useEffect, useState } from "react";
 import * as S from "./styles";
 import axios from "axios";
+import { Card } from "antd";
 
 function IpInformation(props) {
   const { open, close, ip } = props;
   const [info, setInfo] = useState("");
+  const [comlete, setComplete] = useState(false);
 
   useEffect(() => {
+    setComplete(false);
     const response = axios
       /* .get(`/search/ip?ip=${ip}`) */
       .get(`/search/ip?ip=91.236.51.44`)
       .then(function (response) {
-        console.log("data", response.data);
-        setInfo(response.data);
+        setInfo(response.data?.shodan);
+        setComplete(true);
       })
       .catch(function (error) {
         console.log(error);
       });
-  });
+  }, [ip]);
 
   return (
     <S.CustomModal
@@ -30,7 +33,9 @@ function IpInformation(props) {
       width={900}
       bodyStyle={{ height: 430 }}
     >
-      <S.ModalContent>{ip}</S.ModalContent>
+      <S.ModalContent>
+        {comlete ? { ip } : <Card loading style={{ height: 400 }} />}
+      </S.ModalContent>
     </S.CustomModal>
   );
 }
