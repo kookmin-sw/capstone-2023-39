db7407ee5c6fe40d0563e80023b5e425b9329ca1
dan-2ee <ekdms7273@naver.com>
2023-04-30 12:20:13 +0900

    feat: add bitcoin data

--- front-end/src/components/BitcoinTable/index.js
+++ front-end/src/components/BitcoinTable/index.js
@@ -18,6 +18,7 @@ function BitcoinTable() {
         position: "relative",
         width: "100%",
         height: "100%",
+        minHeight: "100%",
       }}
     >
       <S.TableContainer
--- front-end/src/components/BitcoinTable/testdata.js
+++ front-end/src/components/BitcoinTable/testdata.js
@@ -1,12 +1,12 @@
 export const columns = [
   {
-    title: "외부 IP",
-    dataIndex: "ip",
+    title: "Pool Name",
+    dataIndex: "name",
     align: "center",
   },
   {
-    title: "외부 Domain Name",
-    dataIndex: "domain",
+    title: "Pool Ip",
+    dataIndex: "ip",
     align: "center",
   },
   {
@@ -16,11 +16,60 @@ export const columns = [
   },
 ];
 
-export const testdata = [];
-for (let i = 0; i < 50; i++) {
-  testdata.push({
-    ip: (Math.random() * i).toFixed(9),
-    domain: `domain ${i}`,
-    count: Math.random().toFixed(1),
-  });
-}
+export const testdata = [
+  {
+    name: "xmr.2miners.com",
+    ip: "15.197.142.173",
+    count: 680,
+  },
+  {
+    name: "vegas-backup.xmrpool.net",
+    ip: "141.94.96.71",
+    count: 22,
+  },
+  {
+    name: "xevan.mine.blazepool.com",
+    ip: "3.64.163.50",
+    count: 29,
+  },
+  {
+    name: "pool.supportxmr.com",
+    ip: "141.94.96.195",
+    count: 19,
+  },
+  {
+    name: "xerom-eu.upool.in",
+    ip: "199.59.243.223",
+    count: 10,
+  },
+  {
+    name: "btg.pool.gold",
+    ip: "13.248.169.48",
+    count: 7,
+  },
+  {
+    name: "donate.v2.xmrig.com",
+    ip: "199.247.27.41",
+    count: 6,
+  },
+  {
+    name: "us1.fairpool.pro",
+    ip: "82.192.82.226",
+    count: 2,
+  },
+  {
+    name: "etp.sandpool.org",
+    ip: "162.210.199.87",
+    count: 3,
+  },
+  {
+    name: "mine.ryo.fairpool.xyz",
+    ip: "81.171.28.44",
+    count: 3,
+  },
+  {
+    name: "pool.joys.digital",
+    ip: "34.124.149.177",
+    count: 2,
+  },
+];
