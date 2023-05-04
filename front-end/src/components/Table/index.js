d969205f9f73e9d8f3f5c0b753775d5c73960127
dan-2ee <ekdms7273@naver.com>
2023-05-05 01:54:54 +0900

    feat: change date format using dayjs

--- front-end/src/components/Table/index.js
+++ front-end/src/components/Table/index.js
@@ -3,16 +3,26 @@ import BitcoinTable from "./BitcoinTable";
 import { DatePicker } from "antd";
 import * as S from "./styles";
 import { useState } from "react";
+import dayjs from "dayjs";
 
 function Table() {
   const { RangePicker } = DatePicker;
-  const [date, setDate] = useState({ startDate: "", endDate: "" });
+  const [date, setDate] = useState({
+    startDate: dayjs(),
+    endDate: dayjs(),
+  });
+
+  const dateFormat = "YYYY-MM-DD";
   return (
     <div>
       <RangePicker
         allowClear={false}
-        //disabledDate={(date: Date) => isBefore(new Date(), date)}
-        //value={[new Date(startDate), new Date(endDate)]}
+        //disabledDate={(date) => isBefore(new Date(), date)}
+        //value={[new Date(), new Date()]}
+        defaultValue={[
+          dayjs(date.startDate, dateFormat),
+          dayjs(date.startDate, dateFormat),
+        ]}
         size="large"
         style={{ width: "50%" }}
         onCalendarChange={(_, date) => {
