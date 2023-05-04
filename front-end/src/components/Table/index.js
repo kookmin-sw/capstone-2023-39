3d7f24ac05563282fd7433a0b56a9c0a2d989780
dan-2ee <ekdms7273@naver.com>
2023-05-05 01:40:10 +0900

    feat: add date picker for table

--- front-end/src/components/Table/index.js
+++ front-end/src/components/Table/index.js
@@ -1,14 +1,33 @@
 import HybridTable from "./HybridTable";
 import BitcoinTable from "./BitcoinTable";
-
+import { DatePicker } from "antd";
 import * as S from "./styles";
+import { useState } from "react";
 
 function Table() {
+  const { RangePicker } = DatePicker;
+  const [date, setDate] = useState({ startDate: "", endDate: "" });
   return (
-    <S.TableContainer>
-      <HybridTable />
-      <BitcoinTable />
-    </S.TableContainer>
+    <div>
+      <RangePicker
+        allowClear={false}
+        //disabledDate={(date: Date) => isBefore(new Date(), date)}
+        //value={[new Date(startDate), new Date(endDate)]}
+        size="large"
+        style={{ width: "50%" }}
+        onCalendarChange={(_, date) => {
+          const [startDate, endDate] = date;
+          setDate({
+            startDate: startDate,
+            endDate: endDate,
+          });
+        }}
+      />
+      <S.TableContainer>
+        <HybridTable />
+        <BitcoinTable />
+      </S.TableContainer>
+    </div>
   );
 }
 
