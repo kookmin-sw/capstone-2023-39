4bd25c15517f718b8065d31765ef7b8b1acba736
chanjin1998 <chanjin@kookmin.ac.kr>
2023-05-12 01:03:02 +0900

    feat: detect page

---
+++ front-end/src/pages/Detect/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const BodyContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  flex-direction: row;
+  display: flex;
+`;
+
+export const DetectContainer = styled.div`
+  display: flex;
+  flex-direction:column;
+  width: 100%;
+  height: 70%;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+  background: #f8fafd;
+`;
