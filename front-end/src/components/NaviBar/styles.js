8c04e09e1efe6694646276c6885132d5aac104e8
chanjin1998 <chanjin@kookmin.ac.kr>
2023-05-12 01:06:17 +0900

    style: modify navibar design

--- front-end/src/components/NaviBar/styles.js
+++ front-end/src/components/NaviBar/styles.js
@@ -2,21 +2,34 @@ import styled from "styled-components";
 
 export const NaviContainer = styled.div`
   width: 5%;
-  background-color:#000;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color:#897EDB;
   display: flex;
   flex-direction: column;
 `;
-
-export const TopButton = styled.button`
+export const LogoButton = styled.button`
   width: 100%;
   height: 70px;
-  background-color: #000;
+  color: #fff;
+  background-color: #897EDB;
   border: none;
+  border-bottom: 0.5px solid #fff;
 `;
 
-export const BottomButton = styled.button`
+export const RouteButton = styled.button`
   width: 100%;
   height: 70px;
-  background-color: #000;
+  color: #fff;
+  background-color: #897EDB;
   border: none;
+  border-bottom: 0.5px solid #fff;
+  cursor: pointer;
+  transition: 0.3s all;
+   &:hover{
+     transform:scale(1.1);
+     border:none;
+   }
 `;
