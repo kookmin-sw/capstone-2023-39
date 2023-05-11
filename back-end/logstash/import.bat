93d835dfd2b50be3ac1530db05bebdd835c6b4ff
leejoowon20151334 <jwlee51@kookmin.ac.kr>
2023-05-11 22:59:18 +0900

    windows scheduler 관련 bat 추가

---
+++ back-end/logstash/import.bat
@@ -0,0 +1,14 @@
+:: 경로
+set _dir=C:\elk_input
+
+:: 데이터팀 import
+copy %_dir%\data\*.csv %_dir%\data\bak\
+move %_dir%\data\*.csv %_dir%\data\processing\
+forfiles /p %_dir%\data\bak /m *.csv /d -3 /c "cmd /c del @file"
+forfiles /p %_dir%\data\log /m *.log /d -3 /c "cmd /c del @file"
+
+:: 모델팀 import
+copy %_dir%\coin\*.csv %_dir%\coin\bak\
+move %_dir%\coin\*.csv %_dir%\coin\processing\
+forfiles /p %_dir%\coin\bak /m *.csv /d -3 /c "cmd /c del @file"
+forfiles /p %_dir%\coin\log /m *.log /d -3 /c "cmd /c del @file"
