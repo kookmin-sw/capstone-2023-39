63a25f49c5f656c04736d8b6b9706f8bbee3377f
chanjin1998 <chanjin@kookmin.ac.kr>
2023-05-12 01:03:32 +0900

    feat: rename statistics

---
+++ front-end/src/pages/Statistics/statistics.js
@@ -0,0 +1,19 @@
+import React from "react";
+import DashboardBottom from "../../components/Dashboard/Bottom";
+import NaviBar from "../../components/NaviBar";
+import { BodyContainer,TableContainer } from "./styles";
+import Table from "../../components/Table";
+function Statistics() {
+  return (
+    <BodyContainer>
+      <NaviBar/>
+      
+      <TableContainer>
+        <Table />
+        <DashboardBottom />
+      </TableContainer>
+    </BodyContainer>
+  );
+}
+
+export default Statistics;
---
+++ front-end/src/pages/Statistics/styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const BodyContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  flex-direction: row;
+  display: flex;
+  /* flex-wrap:wrap; */
+`;
+
+export const TableContainer = styled.div`
+  display: flex;
+  flex-direction:column;
+  width: 100%;
+  height: 70%;
+  align-items: center;
+  justify-content: center;
+  margin-left: 70;
+  padding: 10px;
+  background: #f8fafd;
+`;
