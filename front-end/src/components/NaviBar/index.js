c122b8a99a83e4f67bcd8596b2d3ffe79d8fde1c
dan-2ee <ekdms7273@naver.com>
2023-05-16 17:16:25 +0900

    style: 메뉴바 border

--- front-end/src/components/NaviBar/index.js
+++ front-end/src/components/NaviBar/index.js
@@ -1,31 +1,49 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { NaviContainer, RouteButton, LogoButton} from "./styles";
-import 'boxicons';
+import { NaviContainer, RouteButton, LogoButton } from "./styles";
+import "boxicons";
 
 function NaviBar() {
   return (
     <NaviContainer>
-      <LogoButton>
-        <box-icon name='react' type='logo' animation='tada' flip='vertical' color='#ffffff'></box-icon> <br />
+      <RouteButton>
+        <box-icon
+          name="react"
+          type="logo"
+          animation="tada"
+          flip="vertical"
+          color="#ffffff"
+        ></box-icon>{" "}
+        <br />
         39조
-      </LogoButton>
+      </RouteButton>
       <RouteButton>
         <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-          <box-icon name='search-alt' color='#ffffff' ></box-icon><br />
+          <box-icon name="search-alt" color="#ffffff"></box-icon>
+          <br />
           악성 IP
         </Link>
       </RouteButton>
       <RouteButton>
-        <Link to="/statistics" style={{ textDecoration: "none", color: "white" }}>
-        <box-icon name='bar-chart-alt-2' type='solid' color='#ffffff' ></box-icon> <br />
+        <Link
+          to="/statistics"
+          style={{ textDecoration: "none", color: "white" }}
+        >
+          <box-icon
+            name="bar-chart-alt-2"
+            type="solid"
+            color="#ffffff"
+          ></box-icon>{" "}
+          <br />
           IP 분석
         </Link>
       </RouteButton>
       <RouteButton>
         <Link to="/detect" style={{ textDecoration: "none", color: "white" }}>
-        <box-icon name='error' color='#ffffff' ></box-icon> <br />
-          보안탐지<br/>대시보드
+          <box-icon name="error" color="#ffffff"></box-icon> <br />
+          보안탐지
+          <br />
+          대시보드
         </Link>
       </RouteButton>
     </NaviContainer>
