20631206e450b69c63ea62e872dd5c5d7efb8a31
dan-2ee <ekdms7273@naver.com>
2023-05-16 20:42:55 +0900

    feat: change bubble size and content

--- front-end/src/pages/Home/home.js
+++ front-end/src/pages/Home/home.js
@@ -2,19 +2,57 @@ import React from "react";
 import NaviBar from "../../components/NaviBar";
 import * as S from "./styles";
 import { GlobalStyle } from "../../fonts/fonts";
+import user from "../../imgs/user.png";
+import useScrollFadeIn from "../../hooks/useScrollFadeIn";
+
+const ShowProject = () => {
+  const questionItem = useScrollFadeIn("up", 1, 0);
+  const answerItemStart = useScrollFadeIn("up", 1, 0);
+  const answerItemMid = useScrollFadeIn("up", 1, 0);
+  const answerItemEnd = useScrollFadeIn("up", 1, 0);
+
+  return (
+    <>
+      <S.questionContainer {...questionItem}>
+        <S.profileImg src={user} />
+        <S.speechBubble direction="left">
+          {"어떤 프로젝트인지 궁금해요 🤔"}
+        </S.speechBubble>
+      </S.questionContainer>
+      <S.answerContainer>
+        <S.bubbleBox {...answerItemStart}>
+          <S.speechBubble direction="right">
+            {
+              "저희 팀은 **국민대학교 내부 IP**를 클라이언트로 설정하여 \n 외부로부터 들어오는 공격을 탐지하는 기술을 개발하였습니다 🚀 \n \n해당 기술은 개발 단계에서 끝나는 것이 아닌, \n실제 **국민대학교** 관계자들이 본 기술을 쉽게 접근하고 사용할 수 있도록 \n서비스화하는 것을 목표로 합니다."
+            }
+          </S.speechBubble>
+        </S.bubbleBox>
+        <S.bubbleBox {...answerItemMid}>
+          <S.speechBubble direction="right">
+            {
+              "더불어 해당 서비스 외에도 다양한 기능들을 추가하여 \n국민대학교 관계자에게 보안에 대한 관심을 높이고 중요성을 부각시키려고 합니다 😀"
+            }
+          </S.speechBubble>
+        </S.bubbleBox>
+      </S.answerContainer>
+    </>
+  );
+};
 
 function Home() {
   return (
     <S.BodyContainer>
       <NaviBar></NaviBar>
-      <GlobalStyle />
       <S.MainContainer>
+        <GlobalStyle />
         <S.titleContainer>
           <S.subTitle>{`국민대학교를 위한 네트워크 이상탐지 기술 개발 및 서비스화`}</S.subTitle>
-          <S.mainTitle>
-            Search Engine for the Internet of Everything !
-          </S.mainTitle>
+          <S.mainTitle>Search ip for the Kookmin of Everything</S.mainTitle>
+          {/*<S.MapImg src={worldMap} />*/}
         </S.titleContainer>
+        <S.ShowContainer>
+          <ShowProject />
+        </S.ShowContainer>
       </S.MainContainer>
     </S.BodyContainer>
   );
--- front-end/src/pages/Home/styles.js
+++ front-end/src/pages/Home/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const BodyContainer = styled.div`
   width: 100vw;
-  height: 100vh;
+  height: 100%;
   display: flex;
   flex-direction: row;
   background: #000;
@@ -19,29 +19,99 @@ export const MainContainer = styled.div`
 `;
 
 export const titleContainer = styled.div`
-  width: 80%;
-  height: fit-content;
-  text-align: left;
+  width: 90%;
+  height: 100%;
+  text-align: center;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   display: flex;
   flex-direction: column;
+  margin-top: 20%;
 `;
 
 export const mainTitle = styled.div`
-  font-size: 75px;
+  font-size: 64px;
   font-weight: 400;
   font-family: "GangwonEduPowerExtraBoldA";
-  line-height: 100px;
+  line-height: 105px;
   color: #fff;
   white-space: pre-line;
+  position: relative;
 `;
 
 export const subTitle = styled.div`
   white-space: pre-line;
-  font-size: 30px;
+  font-size: 28px;
   font-family: "AppleSDGothicNeoL";
-  line-height: 50px;
+  line-height: 30px;
   color: #fff;
+  opacity: 0.7;
+  position: relative;
+`;
+
+export const MapImg = styled.img`
+  width: 612px;
+  height: 612px;
+  align-items: flex-end;
   opacity: 0.8;
+  position: absolute;
+  margin-top: 30%;
+  margin-left: 40%;
+`;
+
+export const ShowContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-left: 60px;
+`;
+
+export const questionContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 90%;
+  height: 60%;
+  margin-top: 20%;
+  align-items: center;
+`;
+
+export const answerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 90%;
+  height: fit-content;
+  align-items: flex-end;
+`;
+
+export const profileImg = styled.img`
+  width: 100px;
+  height: 100px;
+  align-items: flex-start;
+  margin-right: 2%;
+`;
+export const bubbleBox = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+`;
+
+export const speechBubble = styled.div`
+  width: ${(props) =>
+    props.direction === "left" ? "fit-content" : "fit-content"};
+  height: fit-content;
+  background-color: ${(props) =>
+    props.direction === "left" ? "#505254" : "#3476f6"};
+  border-radius: 100px;
+  white-space: pre-line;
+  font-family: "AppleSDGothicNeoL";
+  font-size: 19px;
+  line-height: ${(props) => (props.direction === "left" ? "80px" : "30px")};
+  color: ${(props) => (props.direction === "left" ? "#FFF" : "#FFFFFF")};
+  padding: ${(props) => (props.direction === "left" ? "0 6%" : "2% 6%")};
+  margin-bottom: ${(props) => (props.direction === "left" ? "0" : "3%")};
 `;
