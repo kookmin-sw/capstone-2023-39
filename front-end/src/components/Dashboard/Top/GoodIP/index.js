3ac9544c193d51b7f18324c554a75a32bedf1bae
chanjin1998 <chanjin@kookmin.ac.kr>
2023-05-21 00:54:14 +0900

    style: 크기 조절

--- front-end/src/components/Dashboard/Top/GoodIP/index.js
+++ front-end/src/components/Dashboard/Top/GoodIP/index.js
@@ -6,7 +6,7 @@ function DashboardGoodIp() {
     <Iframe
       styles={{ marginLeft: "4%" }}
       src="http://203.246.112.139:15601/app/dashboards#/view/a8e5fe80-dd2f-11ed-bf4f-d1ab119432ec?embed=true&_g=(refreshInterval%3A(pause%3A!t%2Cvalue%3A60000)%2Ctime%3A(from%3Anow-15m%2Cto%3Anow))&show-query-input=true&show-time-filter=true&hide-filter-bar=true"
-      height="1100"
+      height="800"
       width="97%"
       frameBorder={0}
     ></Iframe>
