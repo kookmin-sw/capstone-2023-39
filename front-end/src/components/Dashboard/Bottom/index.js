a7846294761fe35f6b1f2f8c9707ca16c8ed1e76
chanjin1998 <chanjin@kookmin.ac.kr>
2023-05-21 22:24:47 +0900

    feat: filter bar 추가

--- front-end/src/components/Dashboard/Bottom/index.js
+++ front-end/src/components/Dashboard/Bottom/index.js
@@ -4,7 +4,7 @@ import Iframe from "react-iframe";
 function DashboardBottom() {
   return (
     <Iframe
-      src="http://203.246.112.139:15601/app/dashboards#/view/760bc710-dae6-11ed-8952-470f17d5498f?embed=true&_g=(refreshInterval%3A(pause%3A!t%2Cvalue%3A60000)%2Ctime%3A(from%3Anow-15m%2Cto%3Anow))&show-query-input=true&show-time-filter=true&hide-filter-bar=true"
+      src="http://203.246.112.139:15601/app/dashboards#/view/760bc710-dae6-11ed-8952-470f17d5498f?embed=true&_g=(refreshInterval%3A(pause%3A!t%2Cvalue%3A60000)%2Ctime%3A(from%3Anow-15m%2Cto%3Anow))&show-query-input=true&show-time-filter=true"
       height="860"
       width="99%"
       frameBorder={0}
