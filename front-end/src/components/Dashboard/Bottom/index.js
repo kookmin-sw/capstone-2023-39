95e692ccc71b02b25be954cf4dd66bab67b01d06
dan-2ee <ekdms7273@naver.com>
2023-05-23 20:29:20 +0900

    style: dashboard 길이

--- front-end/src/components/Dashboard/Bottom/index.js
+++ front-end/src/components/Dashboard/Bottom/index.js
@@ -5,7 +5,7 @@ function DashboardBottom() {
   return (
     <Iframe
       src="http://203.246.112.139:15601/app/dashboards#/view/760bc710-dae6-11ed-8952-470f17d5498f?embed=true&_g=(refreshInterval%3A(pause%3A!t%2Cvalue%3A60000)%2Ctime%3A(from%3Anow-15m%2Cto%3Anow))&show-query-input=true&show-time-filter=true"
-      height="860"
+      height="880"
       width="99%"
       frameBorder={0}
       styles={{ background: "#f8fafd" }}
--- front-end/src/components/Dashboard/Detect/index.js
+++ front-end/src/components/Dashboard/Detect/index.js
@@ -6,7 +6,7 @@ function Detect() {
     <Iframe
       //styles={{ marginLeft: 60 }}
       src="http://203.246.112.139:15601/app/dashboards#/view/21ba2b90-efed-11ed-826a-41329daeef14?embed=true&_g=(refreshInterval%3A(pause%3A!t%2Cvalue%3A60000)%2Ctime%3A(from%3Anow-15m%2Cto%3Anow))&show-query-input=true&show-time-filter=true"
-      height="1300"
+      height="1640"
       width="100%"
       frameBorder={0}
     ></Iframe>
