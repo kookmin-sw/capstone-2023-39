f2c295c63cc703e10ff72c1e68e649273882bbe6
dan-2ee <ekdms7273@naver.com>
2023-05-24 13:12:35 +0900

    주석 수정

--- front-end/src/components/Table/index.js
+++ front-end/src/components/Table/index.js
@@ -9,7 +9,6 @@ function Table() {
   const dateFormat = "YYYY-MM-DD";
   const { RangePicker } = DatePicker;
   const [date, setDate] = useState({
-    // 오늘 날짜로 생성
     startDate: dayjs().format(dateFormat),
     endDate: dayjs().format(dateFormat),
   });
@@ -18,8 +17,6 @@ function Table() {
     <S.Wrap>
       <RangePicker
         allowClear={false}
-        //disabledDate={(date) => isBefore(new Date(), date)}
-        //value={[new Date(), new Date()]}
         defaultValue={[
           dayjs(date.startDate, dateFormat),
           dayjs(date.startDate, dateFormat),
